d351c6b7727490b5e93a0166f8ef6d00c160bf68
Max Whitney <mwhitney@cs.sfu.ca>
2012-05-24 15:24:20 -0700

    Added a bibtex feature for in-place bibtex lists.

--- _plugins/bibtex.rb
+++ _plugins/bibtex.rb
@@ -25,10 +25,17 @@
 # Options in the 'bibtex' configuration section:
 #   citation_style: citation style for citeproc
 #   citation_locale: citation locale for citeproc
-#   skip_keys: space separated bibtex keys to remove before showing a reference
+#   out_skip_keys: space separated bibtex keys to remove before showing a
+#     reference as html
+#   bibtex_skip_keys: space separated bibtex keys to remove before showing a
+#     reference as a bibtex string
 #   sort_keys: space separated bibtex keys to sort on (see below)
 #   page_dir: the directory to contain individual entry pages
 #   page_style: the style for individual entry pages
+#   in_default_bib_type: the bibtex type to use to refer to the default
+#     bibliography from an in-place reference list
+#   append_ref: HTML to insert at the end of each formatted reference; can
+#     contain liquid code, and the variable 'item' will be set
 #
 
 require 'bibtex'
@@ -94,12 +101,14 @@ module Jekyll
     end
 
     # Add formatted output to an entry.
-    def self.format_entry(bibtex, out_skip_keys, bibtex_skip_keys, cite_style, cite_locale)
+    def self.format_entry(bibtex, out_skip_keys, bibtex_skip_keys, append_ref, cite_style, cite_locale)
       html_bibtex = bibtex.dup
       bibtex_bibtex = bibtex.dup
       # HTML formatted reference.
       remove_skip_keys(html_bibtex, out_skip_keys)
-      bibtex['html'] = CiteProc.process(html_bibtex.to_citeproc, :style => cite_style, :locale => cite_locale, :format => 'html')
+      html = CiteProc.process(html_bibtex.to_citeproc, :style => cite_style, :locale => cite_locale, :format => 'html')
+      bibtex['html'] = html
+      bibtex['exthtml'] = [html, append_ref].join if append_ref != nil
       # Filtered bibtex as string.
       remove_skip_keys(bibtex_bibtex, bibtex_skip_keys)
       bibtex['bibtex'] = bibtex_bibtex.to_s
@@ -136,28 +145,54 @@ module Jekyll
         out_skip_keys = (config['out_skip_keys'] || "").split()
         bibtex_skip_keys = (config['bibtex_skip_keys'] || "").split()
         sort_keys = (config['sort_keys'] || "-year -monthnum title order").split()
+        append_ref = config['append_ref']
         page_dir = config['page_dir']
 
-        filename = opts[:filename]
-        on_default = false
-        if filename == nil then
-          filename = config['file']
+        # We can load either from a string, a file by name, or (if neither is
+        # given) then from the default bibliography file.
+        if opts[:string] != nil then
+          on_default = false
+          filename = nil
+          bibtex_string = opts[:string]
+        elsif opts[:filename] != nil then
+          on_default = false
+          filename = File.join(source, opts[:filename])
+          bibtex_string = nil
+        else
+          filename = File.join(source, config['file'])
+          bibtex_string = nil
           on_default = true
         end
-        filename = File.join(source, filename)
 
-        if @bib_cache.key?(filename) then
-          @bib_cache[filename]
-        else
-          bib = File.open(filename) { |f| BibTeX.parse(f) }
-          bib = bib.map { |bt| bt } # get a plain list
-          bib.each_with_index do |bibtex, i|
-            format_entry(bibtex, out_skip_keys, bibtex_skip_keys, cite_style, cite_locale)
-            augment_entry(i, bibtex, on_default, page_dir, cite_style, cite_locale)
+        bib =
+          if filename != nil and @bib_cache.key?(filename) then
+            @bib_cache[filename]
+          else
+            bib_list =
+              if bibtex_string == nil then File.open(filename) { |f| BibTeX.parse(f) }
+              else BibTeX.parse(bibtex_string)
+              end
+            bib_list = bib_list.map { |bt| bt } # get a plain list
+            bib_list.each_with_index do |bibtex, i|
+              augment_entry(i, bibtex, on_default, page_dir, cite_style, cite_locale)
+              format_entry(bibtex, out_skip_keys, bibtex_skip_keys, append_ref, cite_style, cite_locale)
+            end
+            sort_bib(bib_list, sort_keys)
+            bib_lookup = Hash[bib_list.map { |bt| [bt.key, bt] }]
+            bib = [bib_list, bib_lookup]
+            @bib_cache[filename] = bib if filename != nil
+            bib
           end
-          sort_bib(bib, sort_keys)
-          @bib_cache[filename] = bib
-          bib
+
+        # We can get the bibliography either as an ordered list or as a lookup
+        # by key.
+        case opts[:form] || :list
+        when :list
+          bib[0]
+        when :lookup
+          bib[1]
+        else
+          fail "unknown form"
         end
     end
   end
@@ -166,10 +201,19 @@ module Jekyll
   # interface. It's just the bibtex that we store, including the extra sort
   # keys and the formatted output keys.
   class Reference
-    def initialize(bibtex)
+    def initialize(bibtex, context)
       @data = {}
       # We need to get a hash with string keys.
-      bibtex.each_pair { |k, v| @data[k.to_s] = v }
+      bibtex.each_pair { |k, v| @data[k.to_s] = v if k != 'html' }
+      @data['html'] =
+        if context != nil and bibtex['exthtml'] != nil then
+          # If we were given context, then expand the extended html.
+          sub_context = context.dup
+          sub_context['item'] = self
+          Liquid::Template.parse(bibtex['exthtml']).render(sub_context)
+        else
+          bibtex['html']
+        end
     end
 
     def to_liquid
@@ -189,7 +233,7 @@ module Jekyll
     def render(context)
       # Make a list of reference objects available as a liquid variable.
       site = context['site']
-      context['bibliography'] = Jekyll::Bibtex.make_references(site, site['source'], site['bibtex'], :filename => @filename).map { |bt| Reference.new(bt) }
+      context['bibliography'] = Jekyll::Bibtex.make_references(site, site['source'], site['bibtex'], :filename => @filename).map { |bt| Reference.new(bt, context) }
       super
     end
   end
@@ -207,7 +251,7 @@ module Jekyll
       data['title'] = bibtex['title'] != nil ? "#{bibtex['title']}" : 'Publication'
       data['title'] += " (#{bibtex['year']})" if bibtex['year'] != nil
       data['layout'] = layout
-      data['publication'] = Reference.new(bibtex)
+      data['publication'] = Reference.new(bibtex, nil)
     end
   end
 
@@ -229,6 +273,56 @@ module Jekyll
       end
     end
   end
+
+  # Liquid block for in-place bibtex.
+  class ReferenceListBlock < Liquid::Block
+    def initialize(tag_name, param_text, tokens)
+      super
+      params = param_text.split(',').map { |p| p.strip }
+      @show_abstracts = params.include?('abstracts')
+    end
+
+    def parse(tokens)
+      @nodelist = []
+      while token = tokens.shift
+        if token =~ IsTag and token =~ FullToken and block_delimiter == $1
+          end_tag
+          return
+        else
+          @nodelist << token
+        end
+      end
+      assert_missing_delimiation!
+    end
+
+    # Output html.
+    def render(context)
+      site = context['site']
+      config = site['bibtex'] || {}
+      dir = config['page_dir']
+      layout = config['page_layout']
+      in_default_bib_type = config['in_default_bib_type'] || 'indefaultbib'
+      bibtex_string = super
+      if dir != nil and layout != nil then
+        bibtex_bib = Jekyll::Bibtex.make_references(site, site['source'], config, :string => bibtex_string)
+        bibtex_defaults = Jekyll::Bibtex.make_references(site, site['source'], config, :form => :lookup)
+        begin
+          ["<ul>"] +
+            bibtex_bib.map do |bibtex|
+              bibtex = bibtex_defaults[bibtex.key] if bibtex.type.to_s == in_default_bib_type
+              lines = ["<li class=\"referencelist\">", "#{Reference.new(bibtex, context).to_liquid['html']}"]
+              lines << "<blockquote>#{bibtex['abstract']}</blockquote>" if @show_abstracts and bibtex['abstract'] != nil
+              lines << "</li>"
+              lines
+            end.flatten.map { |l| "\t" + l } +
+            ["</ul>"]
+        end.join("\n")
+      else
+        ""
+      end
+    end
+  end
 end
 
 Liquid::Template.register_tag('bibliography', Jekyll::BibliographyBlock)
+Liquid::Template.register_tag('referencelist', Jekyll::ReferenceListBlock)
