3b8f6a78100d6bedbd8c94674eb7d1ad17985419
Max Whitney <mwhitney@cs.sfu.ca>
2012-05-24 17:51:07 -0700

    More flexible featured item system; added a couple of featured items.

--- _plugins/featured.rb
+++ _plugins/featured.rb
@@ -1,11 +1,9 @@
 #
 # Support for a list of featured posts.
-# A list of post file basenames (with or without the extensions) is read from
-# a file specifed by a paramter, or if not given then the 'featured: list_file:'
-# configuration option. Posts may be given 'featured_text' and 'featured_image'
-# variables in front matter. Inside the 'featured' liquid block, the variable
-# 'featured' will contain those posts that are in the list and have both
-# variables set.
+# Featured items are read from a file specifed by a paramter, or if not given
+# then the 'featured: list_file:' configuration option. The file is a YAML
+# list where each item can have "url" (the URL to link to) , "img" (the URL
+# for an image) and "text" properties.
 #
 
 module Jekyll
@@ -19,26 +17,15 @@ module Jekyll
       site = context['site']
       posts = site['posts']
 
-      # Get the list of current featured posts as basenames of post files.
       filename = File.join(site['source'], @filename || site['featured']['list_file'])
-      featured_list =
+      context['featured'] =
           begin
             File.open(filename) do |file|
-              file.readlines.map { |l| l.chomp }
+              YAML::load(file)
             end
           rescue
             []
           end
-
-      # Make a list of featured posts with the required front matter fields, and
-      # put it in a liquid variable.
-      context['featured'] = posts.select do |post|
-          name = post.instance_variable_get(:@name)
-          in_list = (featured_list.include?(name) or featured_list.include?(File.basename(name, '.*')))
-          has_data = (post.data['featured_text'] != nil and post.data['featured_image'] != nil)
-          $stderr.puts "error: #{name} is in a featured list but does not have the needed front matter variables" if in_list and not has_data
-          in_list and has_data
-        end
       super
     end
   end
